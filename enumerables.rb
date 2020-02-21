4db1f5b34554bef9e61c522d3ea7514365aa81d1
kyle-law <junwei_1121@hotmail.com>
2020-02-22 02:44:00 +0800

    my_all? and my_any? methods solved

--- enumerables.rb
+++ enumerables.rb
@@ -11,10 +11,51 @@ module Enumerable
         end
     end
 
-    def my_select
+    def my_select #interesting...
         for i in 0..self.length-1
-            yield
+            arr=[]
+            criteria = yield(self[i])
+            arr << self[i] if criteria
         end
+    end
+
+    def my_all?
+        arr = []
+        self.my_each do |i|
+            condition = yield(i)
+            condition ? arr<<true : arr<<false
+        end
+        puts arr.include?(false) ? false : true
+    end
+
+    def my_any?
+        arr = []
+        self.my_each do |i|
+            condition = yield(i)
+            condition ? arr<<true : arr<<false
+        end
+        puts arr.include?(true)
+    end
+
+    def my_none?
+    end
+
+    def my_count
+    end
+
+    def my_map
+    end
+
+    def my_inject
+    end
+
+    def multiply_els
+    end
+
+    def my_map #proc only
+    end
+
+    def my_map #proc or block
     end
 
 
@@ -25,4 +66,7 @@ class Array
 end
 
 # [1,2,3,4,5].my_each{|n| print n}
-[1,2,3,4,5].my_each_with_index{|a,b| puts"#{a} with index #{b}"}
+# [1,2,3,4,5].my_each_with_index{|a,b| puts"#{a} with index #{b}"}
+# [1,2,3,4,5].my_select {|n| n.even?}
+# [1,2,3,4,5].my_all? {|n| n<10}
+# [1,2,3,4,5].my_any? {|n| n<0}
