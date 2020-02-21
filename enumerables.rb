38c9b0240b9e6a6a18404201bceb930e86d73b87
kyle-law <junwei_1121@hotmail.com>
2020-02-21 21:03:17 +0800

    Added my_each_with_index method

--- enumerables.rb
+++ enumerables.rb
@@ -1,9 +1,22 @@
 module Enumerable
     def my_each
-        for i in 0..self.length
+        for i in 0..self.length-1
             yield self[i]
         end
     end
+
+    def my_each_with_index
+        for i in 0..self.length-1
+            yield(self[i],i)
+        end
+    end
+
+    def my_select
+        for i in 0..self.length-1
+            yield
+        end
+    end
+
 
 end
 
@@ -11,5 +24,5 @@ class Array
     prepend Enumerable
 end
 
-[1,2,3,4,5].my_each{|n| print n}
-
+# [1,2,3,4,5].my_each{|n| print n}
+[1,2,3,4,5].my_each_with_index{|a,b| puts"#{a} with index #{b}"}
